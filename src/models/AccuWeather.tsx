211c5e92e891c17c19870514353c9ad1f2b92509
DorAshkenazi <dor.ashkenazi01@gmail.com>
2021-07-15 14:52:50 +0300

    https fix

--- src/models/AccuWeather.tsx
+++ src/models/AccuWeather.tsx
@@ -1,2 +1,2 @@
 export const APIKey: string = "87jQqS6UY9bJxtNeF1OVLcyCVHj6aAzB";
-export const rootUrl: string = "http://dataservice.accuweather.com/"
+export const rootUrl: string = "https://dataservice.accuweather.com/"
